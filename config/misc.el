dbb5a2d4c737fd3bc9175a5fb241b6a921080d36
Matthew Boston <matthew.boston@gmail.com>
2013-03-29 00:20:01 -0400

    config miscellaneous

---
+++ config/misc.el
@@ -0,0 +1,18 @@
+;; let emacs find homebrews
+(push "/usr/local/bin" exec-path)
+
+;; Save file backups in a temporary directory.
+(setq backup-directory-alist `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
+
+;; All files should end in a newline.
+(setq require-final-newline t)
+
+;; clean un-used buffers at given time
+(midnight-delay-set 'midnight-delay "3:33pm")
+
+;; version control to follow symlinked files
+(setq vc-follow-symlinks t)
+
+;; Emacs is fast for OSX again!
+(setq font-lock-verbose nil)
--- init.el
+++ init.el
@@ -39,24 +39,8 @@
 (load "extends/markdown-mode")
 (load "extends/window-number-mode")
 
-;; let emacs find homebrews
-(push "/usr/local/bin" exec-path)
-
-;; Save file backups in a temporary directory.
-(setq backup-directory-alist `((".*" . ,temporary-file-directory)))
-(setq auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
-
-;; All files should end in a newline.
-(setq require-final-newline t)
-
-;; clean un-used buffers at given time
-(midnight-delay-set 'midnight-delay "3:33pm")
-
-;; version control to follow symlinked files
-(setq vc-follow-symlinks t)
-
-;; Emacs is fast for OSX again!
-(setq font-lock-verbose nil)
+;; miscellaneous configurations
+(load "config/misc")
 
 ;; now, make it pretty.
 (load "config/cosmetics")
