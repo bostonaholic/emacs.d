850ba92e4c637e9e82da7a4174913cca94b2c83d
Matthew Boston <matthew.boston@gmail.com>
2013-03-28 22:08:25 -0400

    fix incorrect marmalade link

--- packages.el
+++ packages.el
@@ -2,7 +2,7 @@
 (add-to-list 'package-archives
              '("gnu" . "http://elpa.gnu.org/packages/") t)
 (add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/package/s") t)
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
