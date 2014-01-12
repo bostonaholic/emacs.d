0f3ccf4ff28a7ddbb5d5a23f6bbf02d3b317c161
Matthew Boston <matthew.boston@gmail.com>
2014-01-11 20:41:27 -0500

    add some configs for cperl-mode

--- extends/cperl-mode.el
+++ extends/cperl-mode.el
@@ -3,3 +3,6 @@
 
 ;; add test files to cperl-mode
 (setq auto-mode-alist (cons '("\\.t$" . cperl-mode) auto-mode-alist))
+(add-to-list 'auto-mode-alist '("\\.p[lm]$" . cperl-mode))
+(add-to-list 'auto-mode-alist '("\\.pod$" . pod-mode))
+(add-to-list 'auto-mode-alist '("\\.tt$" . tt-mode))
