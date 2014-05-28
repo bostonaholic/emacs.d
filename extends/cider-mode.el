67b1d4712a2a0a561dc61815c7e1bea55c50b0e3
Matthew Boston <matthew.boston@gmail.com>
2014-05-28 19:25:43 -0400

    improve clojure and cider modes

--- extends/cider-mode.el
+++ extends/cider-mode.el
@@ -18,3 +18,13 @@
 
 ;; enable paredit in REPL buffer
 (add-hook 'cider-repl-mode-hook 'paredit-mode)
+
+;; ac-nrepl (Auto-complete for the nREPL)
+(add-hook 'cider-mode-hook 'ac-nrepl-setup)
+(add-hook 'cider-repl-mode-hook 'ac-nrepl-setup)
+(add-to-list 'ac-modes 'cider-mode)
+(add-to-list 'ac-modes 'cider-repl-mode)
+
+;; Poping-up contextual documentation
+(eval-after-load "cider"
+  '(define-key cider-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc))
--- extends/clojure-mode.el
+++ extends/clojure-mode.el
@@ -1,3 +1,7 @@
+;; docs
+(add-hook 'clojure-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
+
 ;; use this function to open a repl on heroku deployed apps
 (defun heroku-repl ()
   (interactive)
