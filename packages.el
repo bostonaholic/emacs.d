4db2169ee45ea672932a844dc54ea3e3dcedd8d1
Matthew Boston <matthew.boston@gmail.com>
2014-07-28 21:39:05 -0600

    add package repositories transactionally

--- packages.el
+++ packages.el
@@ -1,8 +1,7 @@
 (require 'package)
 
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 (package-initialize)
 
 (when (not package-archive-contents)
