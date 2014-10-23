c9f16ea11b0010d550621c02abec966860ec08db
Matthew Boston <matthew.boston@gmail.com>
2014-10-23 15:22:49 -0600

    use new melpa.org

--- lisp/packages.el
+++ lisp/packages.el
@@ -2,9 +2,8 @@
 
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ;; ("melpa" . "http://melpa.milkbox.net/packages/")
-                         ("melpa-stable" . "http://melpa-stable.milkbox.net/packages/")
-                         ))
+                         ;; ("melpa" . "http://melpa.org/packages/")
+                         ("melpa-stable" . "http://stable.melpa.org/packages/")))
 (package-initialize)
 
 (when (not package-archive-contents)
