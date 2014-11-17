d1b7441e462bd21bacbfc2403a6f0591a1e8fafc
Matthew Boston <matthew.boston@gmail.com>
2014-11-16 17:27:29 -0700

    2 space tabs for coffee-mode

--- lisp/extends/coffee-mode.el
+++ lisp/extends/coffee-mode.el
@@ -1,1 +1,8 @@
 (add-hook 'coffee-mode-hook 'paredit-mode)
+
+(defun coffee-custom ()
+  "coffee-mode-hook"
+  (make-local-variable 'tab-width)
+  (set 'tab-width 2))
+
+(add-hook 'coffee-mode-hook 'coffee-custom)
