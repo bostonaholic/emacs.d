a18dab29e0e87507712e4ade1402bf2cead2dfc1
Matthew Boston <matthew.boston@gmail.com>
2014-11-20 12:00:11 -0700

    2 spaces for tabs in coffeescript

--- lisp/extends/coffee-mode.el
+++ lisp/extends/coffee-mode.el
@@ -1,8 +1,8 @@
 (add-hook 'coffee-mode-hook 'paredit-mode)
 
-(defun coffee-custom ()
-  "coffee-mode-hook"
-  (make-local-variable 'tab-width)
-  (set 'tab-width 2))
+(eval-after-load 'coffee
+  '(progn (define-key coffee-mode-map "{" 'paredit-open-curly)
+          (define-key coffee-mode-map "}" 'paredit-close-curly)))
 
-(add-hook 'coffee-mode-hook 'coffee-custom)
+;; This gives you a tab of 2 spaces
+(custom-set-variables '(coffee-tab-width 2))
