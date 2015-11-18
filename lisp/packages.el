5f096b6fe7393cf081cf28e64b51f67ce2a370fb
Matthew Boston <matthew.boston@gmail.com>
2015-11-18 08:30:16 -0700

    add (commented) mepla package repository

--- lisp/packages.el
+++ lisp/packages.el
@@ -1,6 +1,7 @@
 (require 'package)
 
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ;; ("melpa" . "https://melpa.org/packages/")
                          ("melpa-stable" . "https://stable.melpa.org/packages/")
                          ("marmalade" . "https://marmalade-repo.org/packages/")))
 (package-initialize)
