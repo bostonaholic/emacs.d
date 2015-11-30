1f7cdb16cf6c302401154060b18237b023358c75
Matthew Boston <matthew.boston@gmail.com>
2015-11-30 15:50:34 -0700

    hooks to compile all emacs lisp files

--- lisp/hooks.el
+++ lisp/hooks.el
@@ -1,5 +1,5 @@
 ;; hooks to compile all emacs lisp files
-;;(add-hook 'emacs-lisp-mode-hook 'msb/after-save-byte-compile-file-hook)
+(add-hook 'emacs-lisp-mode-hook 'msb/after-save-byte-compile-file-hook)
 
 ;; paredit for nrepl
 (add-hook 'nrepl-mode-hook 'paredit-mode)
