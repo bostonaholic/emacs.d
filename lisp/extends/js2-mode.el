6e40d9343fbd71a979561692c1c6bfc2a059d5ad
Matthew Boston <matthew.boston@gmail.com>
2015-11-30 23:09:40 -0700

    improve js2-mode

--- lisp/extends/js2-mode.el
+++ lisp/extends/js2-mode.el
@@ -1,16 +1,18 @@
 ;; JavaScript files are js2-mode
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.json\\'" . json-mode))
 
-(eval-after-load 'js
+(eval-after-load 'js2-mode
   '(progn
+     (add-hook 'js2-mode-hook 'msb/pretty-function)
      (define-key js-mode-map "(" 'paredit-open-round)
      (define-key js-mode-map ")" 'paredit-close-round)
      (define-key js-mode-map "{" 'paredit-open-curly)
      (define-key js-mode-map "}" 'paredit-close-curly)
+     (setq js2-highlight-level 3)
      (setq js-indent-level 2)))
 
-(setq js2-highlight-level 3)
-
-(add-hook 'js-mode-hook 'msb/pretty-function)
-(add-hook 'json-mode-hook 'flymake-json-load)
-(add-hook 'json-mode-hook 'paredit-mode)
+(eval-after-load 'json-mode
+  '(progn
+     (add-hook 'json-mode-hook 'flymake-json-load)
+     (add-hook 'json-mode-hook 'paredit-mode)))
