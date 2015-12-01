9631f5297b0a8c4521cc7b784f0e24b2c4b29cd9
Matthew Boston <matthew.boston@gmail.com>
2015-11-30 21:25:21 -0700

    remove starter-kit-js

--- lisp/extends/js2-mode.el
+++ lisp/extends/js2-mode.el
@@ -13,3 +13,4 @@
 
 (add-hook 'js-mode-hook 'msb/pretty-function)
 (add-hook 'json-mode-hook 'flymake-json-load)
+(add-hook 'json-mode-hook 'paredit-mode)
--- lisp/starter-kit-js.el
+++
@@ -1,8 +0,0 @@
-;;; starter-kit-js.el
-
-(eval-after-load 'js
-  '(progn (define-key js-mode-map "{" 'paredit-open-curly)
-          (define-key js-mode-map "}" 'paredit-close-curly)
-          (setq js-indent-level 2)))
-
-(add-hook 'json-mode-hook 'paredit-mode)
