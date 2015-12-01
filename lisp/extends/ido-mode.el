53f651c6e6f1c3762082335b718975817385bf74
Matthew Boston <matthew.boston@gmail.com>
2015-11-30 20:24:05 -0700

    move ido settings out of starter kit

--- lisp/extends/ido-mode.el
+++ lisp/extends/ido-mode.el
@@ -1,3 +1,12 @@
 (ido-mode t)
 (ido-everywhere t)
 (ido-ubiquitous-mode t)
+
+(setq ido-enable-prefix nil
+      ido-enable-flex-matching t
+      ido-auto-merge-work-directories-length nil
+      ido-create-new-buffer 'always
+      ido-use-filename-at-point 'guess
+      ido-use-virtual-buffers t
+      ido-handle-duplicate-virtual-buffers 2
+      ido-max-prospects 10)
--- lisp/starter-kit.el
+++ lisp/starter-kit.el
@@ -56,13 +56,4 @@
 (show-paren-mode t)
 (setq-default indent-tabs-mode nil)
 
-(setq ido-enable-prefix nil
-      ido-enable-flex-matching t
-      ido-auto-merge-work-directories-length nil
-      ido-create-new-buffer 'always
-      ido-use-filename-at-point 'guess
-      ido-use-virtual-buffers t
-      ido-handle-duplicate-virtual-buffers 2
-      ido-max-prospects 10)
-
 (defalias 'yes-or-no-p 'y-or-n-p)
