64642adca0aa26f7bbbed15eeef0a66b772713e9
Matthew Boston <matthew@matthewboston.com>
2016-12-09 11:04:56 -0700

    add function to sort words in a region

--- init.el
+++ init.el
@@ -89,6 +89,15 @@
 
 (use-package yaml-mode)
 
+(defun sort-words (reverse beg end)
+  "Sort words in region alphabetically, in REVERSE if negative.
+    Prefixed with negative \\[universal-argument], sorts in reverse.
+    The variable `sort-fold-case' determines whether alphabetic case
+    affects the sort order.
+    See `sort-regexp-fields'."
+  (interactive "*P\nr")
+  (sort-regexp-fields reverse "\\w+" "\\&" beg end))
+
 (setq custom-file (expand-file-name "~/.emacs.d/custom.el"))
 (load custom-file t)
 
