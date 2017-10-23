8dfcce5d1a803284c587eb839c3bd2f5d9a66463
Matthew Boston <matthew@matthewboston.com>
2017-10-22 19:41:41 -0600

    use package winum

--- init.el
+++ init.el
@@ -128,6 +128,24 @@
 
 (use-package smex)
 
+(use-package winum
+  :config
+  (setq winum-auto-assign-0-to-minibuffer nil
+        winum-auto-setup-mode-line nil
+        winum-ignored-buffers '(" *which-key*"))
+  (progn
+    (define-key winum-keymap (kbd "M-0") 'winum-select-window-0-or-10)
+    (define-key winum-keymap (kbd "M-1") 'winum-select-window-1)
+    (define-key winum-keymap (kbd "M-2") 'winum-select-window-2)
+    (define-key winum-keymap (kbd "M-3") 'winum-select-window-3)
+    (define-key winum-keymap (kbd "M-4") 'winum-select-window-4)
+    (define-key winum-keymap (kbd "M-5") 'winum-select-window-5)
+    (define-key winum-keymap (kbd "M-6") 'winum-select-window-6)
+    (define-key winum-keymap (kbd "M-7") 'winum-select-window-7)
+    (define-key winum-keymap (kbd "M-8") 'winum-select-window-8)
+    (define-key winum-keymap (kbd "M-9") 'winum-select-window-9)
+    (winum-mode)))
+
 (use-package yaml-mode)
 
 (defun sort-words (reverse beg end)
